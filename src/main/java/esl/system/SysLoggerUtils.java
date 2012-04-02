00e4cf47d3e05d30d10d20cd7948ae165995b90b
Arjun Satish <arjun.satish@gmail.com>
2012-04-02 20:12:01 +0000

    set logger level to INFO

--- src/main/java/esl/system/SysLoggerUtils.java
+++ src/main/java/esl/system/SysLoggerUtils.java
@@ -1,6 +1,8 @@
 package esl.system;
 
 import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 
 public class SysLoggerUtils {
 
@@ -9,6 +11,7 @@ public class SysLoggerUtils {
     public static void initLogger() {
         if ( !isLoggerInitialized ) {
             BasicConfigurator.configure();
+            Logger.getRootLogger().setLevel(Level.INFO);
             isLoggerInitialized = true;
         }
     }
