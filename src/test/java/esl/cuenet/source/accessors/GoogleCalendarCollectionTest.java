db6a5b070fd3b636c06636840af02d336e02fa9a
Arjun Satish <arjun.satish@gmail.com>
2012-04-06 03:47:54 +0000

    added testbase dep

--- src/test/java/esl/cuenet/source/accessors/GoogleCalendarCollectionTest.java
+++ src/test/java/esl/cuenet/source/accessors/GoogleCalendarCollectionTest.java
@@ -3,10 +3,11 @@ package esl.cuenet.source.accessors;
 import com.mongodb.BasicDBObject;
 import org.apache.log4j.Logger;
 import org.junit.Test;
+import test.TestBase;
 
 import java.io.IOException;
 
-public class GoogleCalendarCollectionTest {
+public class GoogleCalendarCollectionTest extends TestBase {
 
     private Logger logger = Logger.getLogger(YahooPlaceFinderTest.class);
 
