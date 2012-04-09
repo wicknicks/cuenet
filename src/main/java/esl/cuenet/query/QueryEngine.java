41b89fb935b67ec2c0c284f576b38ee01d66e59b
Arjun Satish <arjun.satish@gmail.com>
2012-04-09 19:46:52 +0000

    sparql query is now being executed on our data sources.

--- src/main/java/esl/cuenet/query/QueryEngine.java
+++ src/main/java/esl/cuenet/query/QueryEngine.java
@@ -1,16 +1,25 @@
 package esl.cuenet.query;
 
 import com.hp.hpl.jena.graph.Node;
+import com.hp.hpl.jena.ontology.DatatypeProperty;
+import com.hp.hpl.jena.ontology.Individual;
+import com.hp.hpl.jena.ontology.OntClass;
 import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.rdf.model.Literal;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
 import com.hp.hpl.jena.sparql.core.TriplePath;
 import com.hp.hpl.jena.sparql.core.Var;
 import com.hp.hpl.jena.sparql.expr.Expr;
 import com.hp.hpl.jena.sparql.syntax.*;
+import com.hp.hpl.jena.vocabulary.RDF;
 import esl.cuenet.mapper.tree.SourceMapper;
-import esl.cuenet.source.ISource;
-import esl.cuenet.source.SourceMapVisitor;
+import esl.cuenet.source.*;
+import esl.datastructures.graph.relationgraph.IRelationGraph;
+import esl.datastructures.graph.relationgraph.RelationGraphEdge;
 import esl.datastructures.graph.relationgraph.RelationGraphNode;
 import org.apache.log4j.Logger;
 
@@ -33,6 +42,8 @@ public class QueryEngine {
     }
 
     public void execute(String sparqlQuery) {
+
+        String ns = "http://www.semanticweb.org/arjun/cuenet-main.owl";
         Query query = QueryFactory.create(sparqlQuery);
         query.getQueryPattern().visit(new ElementVisitorImpl(queryGraph));
         projectVars = query.getProjectVars();
@@ -41,41 +52,114 @@ public class QueryEngine {
         // convert query triples to individuals and their attributes
         List<RelationGraphNode> allTypedNodes = queryGraph.getAllTypedNodes();
         List<RelationGraphNode> attributeNodes = new ArrayList<RelationGraphNode>();
-        for (RelationGraphNode node: allTypedNodes) {
-            boolean flag = false;
-            for (Var projectVar: projectVars)
-            if (node.name().equalsIgnoreCase(projectVar.toString())) flag = true;
+        for (RelationGraphNode node : allTypedNodes) {
+            boolean flag = true;
+            for (Var projectVar : projectVars)
+                if (node.name().equalsIgnoreCase(projectVar.toString())) flag = false;
             if (flag) attributeNodes.add(node);
         }
 
-        eval();
+        List<Individual> individuals = new ArrayList<Individual>();
+
+        for (RelationGraphNode attrib : attributeNodes) {
+            OntClass cl = model.getOntClass(ns + attrib.name());
+            Individual ind = model.createIndividual(cl);
+            for (RelationGraphEdge edge : queryGraph.getOutgoingEdges(attrib)) {
+                RelationGraphNode dest = queryGraph.getDestinationNode(edge);
+                if (edge.label().equalsIgnoreCase(RDF.type.getURI()))
+                    ind.addProperty(RDF.type, model.getOntClass(dest.name()));
+                else {
+                    Property property = model.getProperty(ns + edge.label());
+                    ind.addLiteral(property, dest.name());
+                }
+            }
+            individuals.add(ind);
+        }
+        eval(individuals);
     }
 
-    private void eval() {
+    private void eval(List<Individual> individuals) {
         if (sourceMapper == null) {
             logger.info("No sources added. Returning");
             return;
         }
 
-        sourceMapper.accept(new SourceVisitor(queryGraph));
+        sourceMapper.accept(new SourceVisitor(queryGraph, individuals));
     }
 
     public class SourceVisitor implements SourceMapVisitor {
         private QueryGraph graph;
+        private List<Individual> inputIndividuals;
 
-        public SourceVisitor(QueryGraph graph) {
+        public SourceVisitor(QueryGraph graph, List<Individual> inputIndividuals) {
             this.graph = graph;
+            this.inputIndividuals = inputIndividuals;
         }
 
         @Override
         public void visit(ISource source) {
             logger.info("Visiting " + source.getName());
-            List<RelationGraphNode> nodes = graph.getAllNodes();
-            for (RelationGraphNode node: nodes) {
-                if (source.getRelationGraph().containsClass(node.name()))
-                    logger.info("Found! " + node.name());
+
+            IRelationGraph relationGraph = source.getRelationGraph();
+            IMapper mapper = source.getMapper();
+
+            List<String> pathExpressions = new ArrayList<String>();
+            List<Literal> literals = new ArrayList<Literal>();
+
+            for (Individual ind : inputIndividuals) {
+
+                Statement s = ind.getProperty(RDF.type);
+                String personType = removeNamespace(s.getObject().toString());
+
+                List<RelationGraphNode> queryNodes = relationGraph.getNodesOfType(personType);
+
+                StmtIterator si;
+                String pathExpr;
+                for (RelationGraphNode queryNode : queryNodes) {
+                    si = ind.listProperties();
+                    while (si.hasNext()) {
+                        Statement st = si.next();
+                        if (!st.getPredicate().getURI().contains("cuenet")) continue;
+                        if (st.getObject().isLiteral()) {
+                            String p = removeNamespace(st.getPredicate().getURI());
+
+                            pathExpr = queryNode.name() + "." + p;
+                            if (!mapper.containsPattern(pathExpr)) continue;
+
+                            Adornment a = mapper.getAdornment(pathExpr);
+
+                            if (a != null && a.type() == Adornment.AdornmentType.Unspecifiable) continue;
+                            logger.info("Query Params: " + pathExpr + ", " + st.getObject().asLiteral());
+                            pathExpressions.add(pathExpr);
+                            literals.add(st.getObject().asLiteral());
+                        }
+                    }
+                }
             }
+
+            if (pathExpressions.size() == 0 || literals.size() == 0 ) {
+                logger.info("Zero input predicates for " + source.getName());
+                return;
+            }
+
+            /* set the query attributes */
+            IResultSet resultsSet = null;
+            try {
+                resultsSet = source.query(pathExpressions, literals);
+            } catch (SourceQueryException e) {
+                e.printStackTrace();
+            } catch (AccesorInitializationException e) {
+                e.printStackTrace();
+            }
+            if (resultsSet != null) logger.info(resultsSet.printResults());
+            else logger.info("NULL Result Set");
         }
+
+        private String removeNamespace(String uri) {
+            int ix = uri.indexOf("#");
+            return uri.substring(ix+1);
+        }
+
     }
 
     public class ElementVisitorImpl implements ElementVisitor {
@@ -89,7 +173,7 @@ public class QueryEngine {
         @Override
         public void visit(ElementPathBlock el) {
             logger.info("Visit: Element Path Block");
-            for (TriplePath tp: el.getPattern().getList()) {
+            for (TriplePath tp : el.getPattern().getList()) {
                 Node sub = tp.getSubject();
                 Node pre = tp.getPredicate();
                 Node obj = tp.getObject();
@@ -100,7 +184,8 @@ public class QueryEngine {
                 } else subNode = graph.createNode(sub.toString());
 
                 RelationGraphNode objNode;
-                if (obj.isLiteral()) objNode = graph.createNode(obj.toString());
+                if (obj.isLiteral())
+                    objNode = graph.createNode((String)obj.getLiteral().getValue());
                 else {
                     if (graph.containsClass(obj.toString())) {
                         objNode = graph.getNodeByName(obj.toString());
@@ -122,49 +207,62 @@ public class QueryEngine {
         @Override
         public void visit(ElementGroup el) {
             logger.info("Visit: Element Group");
-            for (Element m: el.getElements()) {
+            for (Element m : el.getElements()) {
                 m.visit(this);
             }
         }
 
         @Override
-        public void visit(ElementTriplesBlock el) { }
+        public void visit(ElementTriplesBlock el) {
+        }
 
         @Override
-        public void visit(ElementAssign el) { }
+        public void visit(ElementAssign el) {
+        }
 
         @Override
-        public void visit(ElementBind elementBind) { }
+        public void visit(ElementBind elementBind) {
+        }
 
         @Override
-        public void visit(ElementUnion el) { }
+        public void visit(ElementUnion el) {
+        }
 
         @Override
-        public void visit(ElementOptional el) { }
+        public void visit(ElementOptional el) {
+        }
 
         @Override
-        public void visit(ElementDataset el) { }
+        public void visit(ElementDataset el) {
+        }
 
         @Override
-        public void visit(ElementNamedGraph el) { }
+        public void visit(ElementNamedGraph el) {
+        }
 
         @Override
-        public void visit(ElementExists elementExists) { }
+        public void visit(ElementExists elementExists) {
+        }
 
         @Override
-        public void visit(ElementNotExists elementNotExists) { }
+        public void visit(ElementNotExists elementNotExists) {
+        }
 
         @Override
-        public void visit(ElementMinus elementMinus) { }
+        public void visit(ElementMinus elementMinus) {
+        }
 
         @Override
-        public void visit(ElementService el) { }
+        public void visit(ElementService el) {
+        }
 
         @Override
-        public void visit(ElementFetch el) { }
+        public void visit(ElementFetch el) {
+        }
 
         @Override
-        public void visit(ElementSubQuery el) { }
+        public void visit(ElementSubQuery el) {
+        }
     }
 
 }
--- src/test/java/esl/cuenet/query/QueryEngineTest.java
+++ src/test/java/esl/cuenet/query/QueryEngineTest.java
@@ -5,44 +5,51 @@ import com.hp.hpl.jena.ontology.ProfileRegistry;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.vocabulary.RDF;
 import com.hp.hpl.jena.vocabulary.VCARD;
+import esl.cuenet.mapper.parser.MappingParser;
+import esl.cuenet.mapper.parser.ParseException;
+import esl.cuenet.mapper.tree.*;
 import org.junit.Test;
 import test.TestBase;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 public class QueryEngineTest extends TestBase {
 
     @Test
     public void parseSingleQuery() {
-        OntModel model = ModelFactory.createOntologyModel(ProfileRegistry.OWL_LANG);
-
-        String personURI = "http://somewhere/arjun-satish-uri";
-        String givenName = "Arjun";
-        String familyName = "Satish";
-        String fullName = givenName + " " + familyName;
-
-        model.createResource(personURI).addProperty(VCARD.FN, fullName)
-                .addProperty(VCARD.N, model.createResource()
-                        .addProperty(VCARD.Given, givenName)
-                        .addProperty(VCARD.Family, familyName));
-
-        personURI = "http://somewhere/john-smith-uri";
-        givenName = "John";
-        familyName = "Smith";
-        fullName = givenName + " " + familyName;
-
-        model.createResource(personURI).addProperty(VCARD.FN, fullName)
-                .addProperty(VCARD.N, model.createResource()
-                        .addProperty(VCARD.Given, givenName)
-                        .addProperty(VCARD.Family, familyName));
-
-        personURI = "http://somewhere/adarsh-satish-uri";
-        givenName = "Adarsh";
-        familyName = "Satish";
-        fullName = givenName + " " + familyName;
-
-        model.createResource(personURI).addProperty(VCARD.FN, fullName)
-                .addProperty(VCARD.N, model.createResource()
-                        .addProperty(VCARD.Given, givenName)
-                        .addProperty(VCARD.Family, familyName));
+//        OntModel model = ModelFactory.createOntologyModel(ProfileRegistry.OWL_LANG);
+//
+//        String personURI = "http://somewhere/arjun-satish-uri";
+//        String givenName = "Arjun";
+//        String familyName = "Satish";
+//        String fullName = givenName + " " + familyName;
+//
+//        model.createResource(personURI).addProperty(VCARD.FN, fullName)
+//                .addProperty(VCARD.N, model.createResource()
+//                        .addProperty(VCARD.Given, givenName)
+//                        .addProperty(VCARD.Family, familyName));
+//
+//        personURI = "http://somewhere/john-smith-uri";
+//        givenName = "John";
+//        familyName = "Smith";
+//        fullName = givenName + " " + familyName;
+//
+//        model.createResource(personURI).addProperty(VCARD.FN, fullName)
+//                .addProperty(VCARD.N, model.createResource()
+//                        .addProperty(VCARD.Given, givenName)
+//                        .addProperty(VCARD.Family, familyName));
+//
+//        personURI = "http://somewhere/adarsh-satish-uri";
+//        givenName = "Adarsh";
+//        familyName = "Satish";
+//        fullName = givenName + " " + familyName;
+//
+//        model.createResource(personURI).addProperty(VCARD.FN, fullName)
+//                .addProperty(VCARD.N, model.createResource()
+//                        .addProperty(VCARD.Given, givenName)
+//                        .addProperty(VCARD.Family, familyName));
 
 //        String queryString = "SELECT ?x ?givenName ?name" +
 //                " WHERE { " +
@@ -52,18 +59,60 @@ public class QueryEngineTest extends TestBase {
 //                "?name <http://www.w3.org/2001/vcard-rdf/3.0#Given> ?givenName . " +
 //                " FILTER (?x!=<http://somewhere/arjun-satish-uri> && ?givenName!=\"Nujre\") }";
 
+
+        OntModel model = ModelFactory.createOntologyModel();
+
+        try {
+            model.read(new FileReader("/home/arjun/Documents/Dropbox/Ontologies/cuenet-main/cuenet-main.owl"),
+                    "http://www.semanticweb.org/arjun/cuenet-main.owl");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        SourceMapper mapper = getSourceMapper(model, "./mappings/sources.map");
+
         String queryString = "SELECT ?x " +
                 " WHERE { " +
                 "?x <" + RDF.type + "> <http://www.w3.org/1999/02/22-rdf-syntax-ns#event> . " +
-                "?p <http://www.semanticweb.org/arjun/cuenet-main.owl#participant-of> ?x ." +
-                "?p <" + RDF.type + "> <http://www.w3.org/1999/02/22-rdf-syntax-ns#person> ." +
+                "?p <http://www.semanticweb.org/arjun/cuenet-main.owl#participant-in> ?x ." +
+                "?p <" + RDF.type + "> <http://www.semanticweb.org/arjun/cuenet-main.owl#person> ." +
+                "?p <http://www.semanticweb.org/arjun/cuenet-main.owl#email> \"arjun.satish@gmail.com\" ." +
                 "?p <http://www.semanticweb.org/arjun/cuenet-main.owl#name> \"Arjun Satish\" }";
         System.out.println(queryString);
 
-        QueryEngine engine = new QueryEngine(model, null);
+
+        QueryEngine engine = new QueryEngine(model, mapper);
         engine.execute(queryString);
 
 
+    }
+
+    private SourceMapper getSourceMapper(OntModel model,String filename) {
+        MappingParser parser = null;
+        try {
+            parser = new MappingParser(new FileInputStream(filename));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        parser.setIParseTreeCreator(new ParseTree(filename));
+        IParseTree tree = null;
+        try {
+            tree = parser.parse_document();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        ParseTreeInterpreter interpreter = new ParseTreeInterpreter(tree);
+        interpreter.setOntologyModel(model);
+        try {
+            interpreter.interpret();
+        } catch (SourceParseException e) {
+            e.printStackTrace();
+        }
+
+        return interpreter.getSourceMapper();
     }
 
 }
