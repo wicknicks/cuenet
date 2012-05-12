3226eb570dc28f5532b8fe42d408f3cc81aa37de
Arjun Satish <arjun.satish@gmail.com>
2012-05-12 19:52:29 +0000

    fixing voting cycle.

--- src/main/java/esl/cuenet/algorithms/firstk/impl/HashIndexedEntityVoter.java
+++ src/main/java/esl/cuenet/algorithms/firstk/impl/HashIndexedEntityVoter.java
@@ -4,7 +4,6 @@ import com.hp.hpl.jena.ontology.Individual;
 import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.Statement;
-import com.hp.hpl.jena.rdf.model.StmtIterator;
 import com.hp.hpl.jena.vocabulary.RDF;
 import esl.cuenet.algorithms.firstk.Vote;
 import esl.cuenet.algorithms.firstk.Voter;
@@ -22,68 +21,42 @@ import java.util.*;
 public class HashIndexedEntityVoter implements Voter {
 
     private Logger logger = Logger.getLogger(EntityVoter.class);
-    private List<String> discoveredEntityURIs = new ArrayList<String>();
+
     private String entityBeingDiscovered = null;
-    private List<String> verifiedEntityURIs = new ArrayList<String>();
-    private HashMap<String, ScoreArray> candidateMap = new HashMap<String, ScoreArray>();
+//    private List<String> discoveredEntityURIs = new ArrayList<String>();
+//    private List<String> verifiedEntityURIs = new ArrayList<String>();
+
+    private HashMap<String, ScoreArray> candidateTable = new HashMap<String, ScoreArray>();
+
+    List<String> projectVarURIs = new ArrayList<String>();
 
     private QueryEngine queryEngine = null;
     private Property nameProperty = null;
+    private Property emailProperty = null;
 
     public HashIndexedEntityVoter(QueryEngine engine, OntModel model) {
         this.queryEngine = engine;
         nameProperty = model.getProperty(Constants.CuenetNamespace + "name");
+        emailProperty = model.getProperty(Constants.CuenetNamespace + "email");
+        projectVarURIs.add(Constants.CuenetNamespace + "person");
     }
 
     @Override
-    public Vote[] vote(EventGraph graph, List<Individual> candidates) {
+    public Vote[] vote(EventGraph graph, List<Individual> doNotUseArg0) {
         List<Entity> graphEntities = graph.getEntities();
-        List<String> projectVarURIs = new ArrayList<String>();
-        projectVarURIs.add(Constants.CuenetNamespace + "person");
-
         logger.info("Entities found: " + graphEntities.size());
 
-        merge(candidates);
+        merge(graphEntities);
 
         for (Entity entity: graphEntities) {
-            String name = getValueFromEntityNode(entity, Constants.Name);
-            String email = getValueFromEntityNode(entity, Constants.Email);
-
-            if (isDiscovered(name)) continue;
+            if (isDiscovered(entity)) continue;
             entityBeingDiscovered = getLiteralValue(entity.getIndividual(), nameProperty);
-
-            String sparqlQuery = "SELECT ?x \n" +
-                    " WHERE { \n" +
-                    "?x <" + RDF.type + "> <" + Constants.CuenetNamespace + "person> .\n" +
-                    "?y <" + RDF.type + "> <" + Constants.CuenetNamespace + "person> .\n" +
-                    "?y <" + Constants.CuenetNamespace + "knows" + ">" + " ?x .\n";
-
-            if (email != null)
-                sparqlQuery += "?y <" + Constants.CuenetNamespace + "email> \"" + email + "\" .\n";
-            if (name != null)
-                sparqlQuery += "?y <" + Constants.CuenetNamespace + "name> \"" + name + "\" .\n";
-
-            sparqlQuery += "}";
-
-            logger.info("Executing Sparql Query: \n" + sparqlQuery);
-            List<IResultSet> relations = queryEngine.execute(sparqlQuery);
-            logger.info("Found Relations from " + relations.size() + " sources for " + name);
-
-
-            for (IResultSet resultSet : relations) {
-                logger.info(resultSet.printResults());
-                IResultIterator resultIterator = resultSet.iterator();
-                while(resultIterator.hasNext()) {
-                    Map<String, List<Individual>> result = resultIterator.next(projectVarURIs);
-                    List<Individual> relatedCandidates = result.get(Constants.CuenetNamespace + "person");
-                    updateScores(entity.getIndividual(), relatedCandidates);
-                }
-            }
-            discoveredEntityURIs.add(getLiteralValue(entity.getIndividual(), nameProperty));
+            discover(entity);
+            addToDiscoveredPile(entityBeingDiscovered);
         }
 
         List<Vote> votes = new ArrayList<Vote>();
-        for(ScoreArray scoreArray: candidateMap.values()) {
+        for(ScoreArray scoreArray: candidateTable.values()) {
             int tot = 0;
             if (isVerified(getLiteralValue(scoreArray.individual, nameProperty))) continue;
             Collection<Integer> scores = scoreArray.scores.values();
@@ -101,32 +74,69 @@ public class HashIndexedEntityVoter implements Voter {
         return rVotes;
     }
 
-    private void merge(List<Individual> candidates) {
-        for (Individual candidate: candidates) {
-            String name = getLiteralValue(candidate, nameProperty);
-            ScoreArray original = candidateMap.get(name);
-            if (original == null) candidateMap.put(name, new ScoreArray(candidate));
-            else merge(original.individual, candidate);
+    private void discover(Entity entity) {
+        String name = getValueFromEntityNode(entity, Constants.Name);
+        String email = getValueFromEntityNode(entity, Constants.Email);
+
+        String sparqlQuery = "SELECT ?x \n" +
+                " WHERE { \n" +
+                "?x <" + RDF.type + "> <" + Constants.CuenetNamespace + "person> .\n" +
+                "?y <" + RDF.type + "> <" + Constants.CuenetNamespace + "person> .\n" +
+                "?y <" + Constants.CuenetNamespace + "knows" + ">" + " ?x .\n";
+
+        if (email != null)
+            sparqlQuery += "?y <" + Constants.CuenetNamespace + "email> \"" + email + "\" .\n";
+        if (name != null)
+            sparqlQuery += "?y <" + Constants.CuenetNamespace + "name> \"" + name + "\" .\n";
+
+        sparqlQuery += "}";
+
+        logger.info("Executing Sparql Query: \n" + sparqlQuery);
+        List<IResultSet> relations = queryEngine.execute(sparqlQuery);
+        logger.info("Found Relations from " + relations.size() + " sources for " + name);
+
+        for (IResultSet resultSet : relations) {
+            logger.info(resultSet.printResults());
+            IResultIterator resultIterator = resultSet.iterator();
+            while(resultIterator.hasNext()) {
+                Map<String, List<Individual>> result = resultIterator.next(projectVarURIs);
+                List<Individual> relatedCandidates = result.get(Constants.CuenetNamespace + "person");
+                updateScores(entity.getIndividual(), relatedCandidates);
+            }
+        }
+    }
+
+    private void merge(Individual ind) {
+        String name = getLiteralValue(ind, nameProperty);
+        ScoreArray scoreArray = candidateTable.get(name);
+        if (scoreArray == null) candidateTable.put(name, new ScoreArray(ind));
+        else merge(scoreArray.individual, ind);
+    }
+
+    private void merge(List<Entity> entities) {
+        for (Entity entity: entities) {
+            merge(entity.getIndividual());
         }
     }
 
     private void updateScores(Individual graphEntityIndividual, List<Individual> relatedCandidates) {
         for (Individual rCandidate: relatedCandidates) {
             String rName = getLiteralValue(rCandidate, nameProperty);
+            ScoreArray original = candidateTable.get(rName);
+            if (original == null) {
+                //original = new ScoreArray(rCandidate);
+                //candidateTable.put(rName, original);
+                continue;
+            }
+
             if (entityBeingDiscovered.equals(rName)) continue;
             if (isVerified(rName)) continue;
+
             String name = getLiteralValue(graphEntityIndividual, nameProperty);
-            ScoreArray original = candidateMap.get(rName);
-            if (original == null) continue;
             Integer score = original.scores.get(name);
             if (score == null) original.scores.put(name, 1);
             else original.scores.put(name, score+1);
         }
-    }
-
-    private boolean isVerified(String name) {
-        for (String u: verifiedEntityURIs) if (u.equals(name)) return true;
-        return false;
     }
 
     private String getValueFromEntityNode(Entity entity, String constant) {
@@ -143,26 +153,29 @@ public class HashIndexedEntityVoter implements Voter {
 
     @Override
     public void addToVerifiedPile(Individual entity) {
-        verifiedEntityURIs.add(getLiteralValue(entity, nameProperty));
+        String name = getLiteralValue(entity, nameProperty);
+        ScoreArray scoreArray = candidateTable.get(name);
+        if (scoreArray == null) {
+            scoreArray = new ScoreArray(entity);
+            candidateTable.put(name, scoreArray);
+        }
+        scoreArray.isVerified = true;
     }
 
-    private void merge(Individual original, Individual newIndividual) {
-//        StmtIterator iter  = newIndividual.listProperties();
-//        while (iter.hasNext()) {
-//            Statement st = iter.next();
-//            if (st.getObject().isLiteral()) continue;
-//            if (!containsProperty(original, st.getPredicate()))
-//                original.addProperty(st.getPredicate(), st.getObject());
-//        }
+    private boolean isVerified(String name) {
+        ScoreArray scoreArray = candidateTable.get(name);
+        return scoreArray.isVerified;
     }
 
-    private boolean containsProperty(Individual original, Property predicate) {
-        return (original.getProperty(predicate) != null);
+    private void addToDiscoveredPile(String name) {
+        ScoreArray scoreArray = candidateTable.get(name);
+        scoreArray.isDiscovered = true;
     }
 
-    private boolean isDiscovered(String name) {
-        for (String u: discoveredEntityURIs) if (u.equals(name)) return true;
-        return false;
+    private boolean isDiscovered(Entity entity) {
+        String name = getLiteralValue(entity.getIndividual(), nameProperty);
+        ScoreArray scoreArray = candidateTable.get(name);
+        return scoreArray.isDiscovered;
     }
 
     private String getLiteralValue(Individual individual, Property property) {
@@ -176,9 +189,20 @@ public class HashIndexedEntityVoter implements Voter {
         public ScoreArray(Individual individual) {
             this.individual = individual;
             this.scores = new HashMap<String, Integer>(10);
+            this.isDiscovered = false;
+            this.isVerified = false;
         }
         public Individual individual;
         public HashMap<String, Integer> scores;
+        public boolean isDiscovered;
+        public boolean isVerified;
     }
 
+    private void merge(Individual original, Individual newIndividual) {
+        String oem = getLiteralValue(original, emailProperty);
+        String nem = getLiteralValue(newIndividual, emailProperty);
+
+        if (oem == null && nem != null)
+            original.addLiteral(emailProperty, nem);
+    }
 }
---
+++ src/test/java/esl/cuenet/algorithms/firstk/impl/HashIndexedEntityVoterTest.java
@@ -0,0 +1,107 @@
+package esl.cuenet.algorithms.firstk.impl;
+
+import com.hp.hpl.jena.ontology.Individual;
+import com.hp.hpl.jena.ontology.OntClass;
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.mongodb.BasicDBObject;
+import esl.cuenet.algorithms.firstk.Vote;
+import esl.cuenet.algorithms.firstk.exceptions.EventGraphException;
+import esl.cuenet.algorithms.firstk.structs.eventgraph.Entity;
+import esl.cuenet.algorithms.firstk.structs.eventgraph.EventGraph;
+import esl.cuenet.mapper.parser.ParseException;
+import esl.cuenet.model.Constants;
+import esl.cuenet.query.QueryEngine;
+import esl.cuenet.query.drivers.mongodb.MongoDB;
+import esl.system.SysLoggerUtils;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import test.TestBase;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HashIndexedEntityVoterTest extends MongoDB {
+
+    private Logger logger = Logger.getLogger(IndexedEntityVoterTest.class);
+    private TestBase.TestAlgorithm testAlgorithm = null;
+
+    public HashIndexedEntityVoterTest() throws IOException, ParseException {
+        super("test");
+        SysLoggerUtils.initLogger();
+        testAlgorithm = new TestBase.TestAlgorithm();
+    }
+
+    @Test
+    public void run1() throws IOException, ParseException, EventGraphException {
+        long start = System.currentTimeMillis();
+        logger.info("Starting Second Test: " + start);
+
+        OntModel model = testAlgorithm.getModel();
+        List<Individual> eventAttendees = new ArrayList<Individual>(500);
+        OntClass personClass = model.getOntClass(Constants.CuenetNamespace + "person");
+        Property nameProperty = model.getProperty(Constants.CuenetNamespace + "name");
+        Property emailProperty = model.getProperty(Constants.CuenetNamespace + "email");
+
+        MongoDB.DBReader cursor = startReader("conf_attendees");
+        BasicDBObject queryObject = new BasicDBObject("url", "http://vldb2009.org/");
+        cursor.query(queryObject);
+        while(cursor.hasNext()) {
+            Individual attendee = personClass.createIndividual();
+            BasicDBObject obj = (BasicDBObject) cursor.next();
+            String name = obj.getString("name");
+            attendee.addLiteral(nameProperty, name);
+            eventAttendees.add(attendee);
+        }
+
+        logger.info("Event Attendees: " + eventAttendees.size());
+
+        HashIndexedEntityVoter indexedVoter = new HashIndexedEntityVoter(new QueryEngine(testAlgorithm.getModel(),
+                testAlgorithm.getSourceMapper()), testAlgorithm.getModel());
+
+        EventGraph graph = new EventGraph(model);
+
+        Entity entity = graph.createPerson();
+        entity.getIndividual().addProperty(nameProperty, "Arjun Satish");
+        entity.getIndividual().addProperty(emailProperty, "arjun.satish@gmail.com");
+        indexedVoter.addToVerifiedPile(entity.getIndividual());
+
+        Vote[] votes = indexedVoter.vote(graph, eventAttendees);
+        for (Vote vote : votes) logger.info(vote.entityID + "  " + vote.score);
+        if (votes.length == 0)  logger.info("0 entities voted!");
+
+        Entity e1 = graph.createPerson();
+        e1.getIndividual().addProperty(nameProperty, "Atish Das Sarma");
+        e1.getIndividual().addProperty(emailProperty, "atish.dassarma@gmail.com");
+        indexedVoter.addToVerifiedPile(e1.getIndividual());
+
+        e1 = graph.createPerson();
+        e1.getIndividual().addProperty(nameProperty, "Danupon Nanongkai");
+        e1.getIndividual().addProperty(emailProperty, "danupon@gmail.com");
+        indexedVoter.addToVerifiedPile(e1.getIndividual());
+
+        e1 = new Entity(personClass.createIndividual());
+        e1.getIndividual().addProperty(nameProperty, "Chen Li");
+        indexedVoter.addToVerifiedPile(e1.getIndividual());
+
+        e1 = graph.createPerson();
+        e1.getIndividual().addProperty(nameProperty, "Ramesh Jain");
+        indexedVoter.addToVerifiedPile(e1.getIndividual());
+
+        e1 = graph.createPerson();
+        e1.getIndividual().addProperty(nameProperty, "Galen Reeves");
+        indexedVoter.addToVerifiedPile(e1.getIndividual());
+
+        e1 = new Entity(personClass.createIndividual());
+        e1.getIndividual().addProperty(nameProperty, "Nicola Onose");
+        indexedVoter.addToVerifiedPile(e1.getIndividual());
+
+        votes = indexedVoter.vote(graph, eventAttendees);
+        for (Vote vote : votes) logger.info(vote.entityID + "  " + vote.score);
+
+        long end = System.currentTimeMillis();
+        logger.info("Terminated in " + (end-start) + " ms.");
+    }
+
+}
