fbaa482f54726a82b06a7b3346b2e60895c0f878
Arjun Satish <arjun.satish@gmail.com>
2012-07-25 20:24:57 +0000

    simple test case to find out which iteration model is better for a list/array.

---
+++ src/test/java/test/FasterIteration.java
@@ -0,0 +1,32 @@
+package test;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Random;
+
+public class FasterIteration {
+
+    @Test
+    public void junk() {
+        int sz = 100000;
+        Random generator = new Random();
+        LinkedList<Integer> ints = new LinkedList<Integer>();
+        for (int i=0; i<sz; i++) ints.add(generator.nextInt());
+
+        long d = System.currentTimeMillis();
+        int tmp= 0;
+        for (int i=0; i<sz; i++) tmp = ints.get(i) + 1;
+        long diff = System.currentTimeMillis() - d;
+        System.out.println("Time (Index Based): " + diff);
+
+
+        d = System.currentTimeMillis();
+        for (int _i: ints) _i += 1;
+        diff = System.currentTimeMillis() - d;
+        System.out.println("Time (Iterator Based): " + diff);
+
+    }
+
+}
