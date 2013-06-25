d80dd16568d5249500a29363a0e339bf477ab612
wicknicks <arjun.satish@gmail.com>
2013-06-24 17:09:20 -0700

    generate random trees with instance ids

--- scripts/merge-data-gen/events.py
+++ scripts/merge-data-gen/events.py
@@ -6,9 +6,11 @@ class Interval:
     self.end = 10000
 
 class NestedEvent:
+  GenCounts = []
   def __init__(self, _id):
     self.eid = _id
     self.subevents = []
+    self.instance = None
 
   def extend(self, parent, subs):
     # print 'extend eid', self.eid
@@ -16,26 +18,40 @@ class NestedEvent:
       self.subevents.extend(subs)
     for e in self.subevents: e.extend(parent, subs)
 
+  def gen_id(self):
+    self.instance = str(self.eid) + '_' + str(NestedEvent.GenCounts[self.eid])
+    NestedEvent.GenCounts[self.eid] += 1
+
   def randomize(self, ne=None, maxsubeventrepeat=3):
-    print 'eid', self.eid, type(self.eid)
-    if ne==None: ne = NestedEvent(self.eid)
+    # print 'eid', self.eid, type(self.eid)
+    if ne==None: 
+      ne = NestedEvent(self.eid)
+      ne.gen_id()
     for sub in self.subevents:
       r = range(random.randint(0, maxsubeventrepeat))
-      print 'R', sub.eid, r
+      # print 'R', sub.eid, r
       for j in r:
         _rsub = NestedEvent(sub.eid)
+        _rsub.gen_id()
         ne.subevents.append(_rsub)
         sub.randomize(_rsub)
     return ne
 
+  def str_eid(self):
+    if self.instance: return str(self.instance)
+    return str(self.eid)
+
   def __str__(self):
-    s = '(' + str(self.eid) 
-    if len(self.subevents) > 0: s += " ->"
-    for e in self.subevents: s += ' ' + str(e)
+    if len(self.subevents) == 0: return self.str_eid()
+    s = '(' + self.str_eid()
+    for e in self.subevents: 
+      s += ' ' + str(e)
+      # if len(s) > 10000: return 'ouch'
     return s + ')'
 
-if __name__ == '__main__':
+def testRandomize():
   ne = NestedEvent(1)
+  NestedEvent.GenCounts = [0] * 10
   ne.extend(1, map(lambda a: NestedEvent(a), [2, 3]))
   ne.extend(2, map(lambda a: NestedEvent(a), [4, 5]))
   ne.extend(3, map(lambda a: NestedEvent(a), [7, 6]))
@@ -45,7 +61,9 @@ if __name__ == '__main__':
   r = ne.randomize()
   print 'NER', r
 
-if __name__ == '__main__2': #don't run this
+#testRandomize()
+
+if __name__ == '__main__':
   interval = Interval()
 
   roadnetfile = open('/data/osm/uci.roadnet')
@@ -68,6 +86,7 @@ if __name__ == '__main__2': #don't run this
   ontfile = open('/data/osm/events.ont')
   line = ontfile.readline().strip()
   ontconfig = map(lambda a: int(a), line.split(','))
+  NestedEvent.GenCounts = [0] * ontconfig[0];
 
   atomicevents = set()
   halfevents = set()
@@ -79,11 +98,12 @@ if __name__ == '__main__2': #don't run this
   for line in ontfile.readlines():
     if line.find('###') == 0: 
       parts = line.split(' ')
-      if parts[1] == 'HALF': halfevents.add(int(parts[2]))
-      if parts[1] == 'FULL': fullevents.add(int(parts[2]))
-      current = NestedEvent(int(parts[2]))
+      eid = int(parts[2])
+      if parts[1] == 'HALF': halfevents.add(eid)
+      if parts[1] == 'FULL': fullevents.add(eid)
+      current = NestedEvent(eid)
       nestedevents.append(current)
-      nestedeventsindex[int(parts[2])] = current
+      nestedeventsindex[eid] = current
     else:
       e, subs = line.split(' -> ')
       e = int(e)
@@ -93,25 +113,40 @@ if __name__ == '__main__2': #don't run this
 
   ontfile.close()
 
-  ne = nestedeventsindex[nestedeventsindex.keys()[0]].randomize()
-  print nestedeventsindex[nestedeventsindex.keys()[0]]
-  print ne
+  # ne = nestedeventsindex[nestedeventsindex.keys()[0]].randomize()
+  # print nestedeventsindex[nestedeventsindex.keys()[0]]
+  # print ne
 
-  # allevents = set()
-  # for i in range(ontconfig[0]): allevents.add(i)
-  # atomicevents = allevents - events
-  # print('ATOM', sorted(atomicevents) , len(nestedevents))
-  # print('HALF', sorted(halfevents), len(halfevents))
-  # print('FULL', sorted(fullevents), len(fullevents))
+  allevents = set()
+  for i in range(ontconfig[0]): allevents.add(i)
+  atomicevents = allevents - events
+  
+  eventfile = open('/data/osm/events.sim', 'w')
+  
+  # for one edge
+  for edgeix in range(10):
+    testedge = edges.keys()[edgeix]
+    eventfile.write('##' + str(testedge) + '\n')
+    print 'testedge:', testedge
+    for node in edges[testedge]:
+      eventfile.write('#' + str(node))
+      eventfile.write('\n')
+      spans = random.randint(2, 5)
+      for i in range(spans):
+        eid = random.randint(0, ontconfig[0]-1)
+        
+        ''' skip large events'''
+        if eid in fullevents: continue
+        # if eid in halfevents: continue
+        
+        if eid in nestedeventsindex: 
+          print eid, node, i*interval.end/spans, (i+1) * interval.end/spans, 'R'
+          eventfile.write(str(eid) + ',' + str(i*interval.end/spans) + ',' + str((i+1) * interval.end/spans))
+          eventfile.write(',' + str(nestedeventsindex[eid].randomize()))
+          eventfile.write('\n')
+        else:
+          print eid, node, i*interval.end/spans, (i+1) * interval.end/spans
+          eventfile.write(str(eid) + ',' + str(i*interval.end/spans)+ ',' + str((i+1) * interval.end/spans))
+          eventfile.write('\n')
 
-  # print('INTERVAL', interval.start, interval.end)
-
-  # # for one edge
-  # testedge = edges.keys()[0]
-  # for node in edges[testedge]:
-  #   spans = random.randint(2, 5)
-  #   for i in range(spans):
-  #     eid = random.randint(0, ontconfig[0]-1)
-  #     print (eid, node, i*interval.end/spans, (i+1) * interval.end/spans, spans)
-  #     if eid in nestedeventsindex: print eid, 'nested'
-  #   print ''
+  eventfile.close()
