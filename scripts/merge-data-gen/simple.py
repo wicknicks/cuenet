f8fb2f0475917c7af860d15eb299db3ddc79ceba
wicknicks <arjun.satish@gmail.com>
2013-07-01 21:36:35 -0700

    simple script to gen instances

---
+++ scripts/merge-data-gen/simple.py
@@ -0,0 +1,32 @@
+import os, random
+
+def getInstanceFile(dirname):
+  files = os.listdir(dirname)
+  c = 1 + len(filter(lambda a: (a.find('instance.sim') >= 0), files))
+  return os.path.join(dirname, 'instance.sim.' + str(c))
+
+
+depth = 10
+countAtLevels = []
+for i in range(depth):
+  countAtLevels.append(random.randint(1, 5))
+
+writer = open(getInstanceFile('/data/osm'), 'w')
+writer.write('/data/osm/uci.roadnet 1 10000\n')
+writer.write('## 171169284\n')
+writer.write('$$ 1822416005\n')
+writer.write('0_0,0,10000000,R\n')
+
+print countAtLevels
+#countAtLevels = [1, 2, 3, 1]
+prod = 1
+for k in xrange(len(countAtLevels)):
+  count = countAtLevels[k]
+  prod = prod * count
+  for i in xrange(prod):
+    #print str(k) + '_' + str(i%(prod/countAtLevels[k])), '->', str(k+1) + '_' + str(i)
+    writer.write(str(k) + '_' + str(i%(prod/countAtLevels[k])) + ' -> ' + str(k+1) + '_' + str(i))
+    writer.write('\n')
+
+writer.write('.\n')
+writer.close()
