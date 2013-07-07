253b6e153d6e2e88a02e35fc05fb7b7e28e1799d
Arjun Satish <arjun.satish@gmail.com>
2013-07-06 23:29:08 -0700

    scripts for selfmerge, bignodemerge

--- scripts/merge-data-gen/simple.py
+++ scripts/merge-data-gen/simple.py
@@ -2,8 +2,8 @@ import os, random
 
 def getInstanceFile(dirname):
   files = os.listdir(dirname)
-  c = 1 + len(filter(lambda a: (a.find('instance.sim.10') >= 0), files))
-  return os.path.join(dirname, 'instance.sim.10.' + str(c))
+  c = 1 + len(filter(lambda a: (a.find('instance.sim.1000000') >= 0), files))
+  return os.path.join(dirname, 'instance.sim.1000000.' + str(c))
 
 
 depth = 10
@@ -11,7 +11,9 @@ countAtLevels = []
 for i in range(depth):
   countAtLevels.append(random.randint(2, 8))
 
-instancefile = getInstanceFile('/home/arjun/data/cuenet/simplemerge')
+instancefile = getInstanceFile('/home/arjun/data/cuenet/nodemerge')
+# instancefile = '/home/arjun/data/cuenet/nodemerge/prime.sim.1000000'
+maxnode = 1000000
 
 writer = open(instancefile, 'w')
 writer.write('/data/osm/uci.roadnet 1 10000 ')
@@ -23,7 +25,7 @@ writer.write('0_0,0,10000000,R\n')
 #countAtLevels = [8, 2, 8, 2, 6, 2, 5, 3, 5, 4]
 
 print countAtLevels, reduce(lambda a, b: a*b, countAtLevels), instancefile
-#countAtLevels = [1, 2, 3, 1]
+
 prod = 1
 for k in xrange(len(countAtLevels)):
   count = countAtLevels[k]
@@ -32,6 +34,9 @@ for k in xrange(len(countAtLevels)):
     #print str(k) + '_' + str(i%(prod/countAtLevels[k])), '->', str(k+1) + '_' + str(i)
     writer.write(str(k) + '_' + str(i%(prod/countAtLevels[k])) + ' -> ' + str(k+1) + '_' + str(i))
     writer.write('\n')
+    maxnode -= 1
+    if maxnode == 0: break
+  if maxnode == 0: break
 
 writer.write('.\n')
 writer.close()
