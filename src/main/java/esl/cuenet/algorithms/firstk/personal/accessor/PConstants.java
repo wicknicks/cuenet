49a245ff15ddb9f99f0c27f46b33382618bd87dc
wicknicks <arjun.satish@gmail.com>
2013-07-17 16:15:06 -0700

    logger

--- src/main/java/esl/cuenet/algorithms/firstk/personal/accessor/PConstants.java
+++ src/main/java/esl/cuenet/algorithms/firstk/personal/accessor/PConstants.java
@@ -2,7 +2,7 @@ package esl.cuenet.algorithms.firstk.personal.accessor;
 
 public class PConstants {
 
-    public final static String DBNAME = "shweta";
-    public final static String IMAGE = "/data/test_photos/shweta/25b6d665d1821678bc53addcd6134a3e";
+    public final static String DBNAME = null;
+    public final static String IMAGE = null;
 
 }
--- src/test/java/esl/cuenet/algorithms/firstk/impl/person/EventContextNetworkTester.java
+++ src/test/java/esl/cuenet/algorithms/firstk/impl/person/EventContextNetworkTester.java
@@ -2,9 +2,12 @@ package esl.cuenet.algorithms.firstk.impl.person;
 
 import esl.cuenet.algorithms.firstk.personal.EventContextNetwork;
 import esl.cuenet.algorithms.firstk.personal.accessor.Candidates;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 public class EventContextNetworkTester {
+
+    Logger logger = Logger.getLogger(EventContextNetworkTester.class);
 
     @Test
     public void unitTest() {
@@ -16,22 +19,22 @@ public class EventContextNetworkTester {
 
         EventContextNetwork network = new EventContextNetwork();
         EventContextNetwork.ECNRef p1 = network.createPerson("name", "ABCD");
-        System.out.println(p1);
+        logger.info(p1);
         EventContextNetwork.ECNRef p2 = network.createPerson("name", "ABCD");
-        System.out.println(p2);
+        logger.info(p2);
         EventContextNetwork.ECNRef p3 = network.createPerson("name", "ABCD");
-        System.out.println(p3);
+        logger.info(p3);
         EventContextNetwork.ECNRef p4 = network.createPerson("name", "EFGH");
-        System.out.println(p4);
+        logger.info(p4);
         EventContextNetwork.ECNRef p5 = network.createPerson("name", "XYZ");
-        System.out.println(p5);
+        logger.info(p5);
 
 
         EventContextNetwork.ECNRef e1 = network.createEvent("photo-capture", 0, 50);
-        System.out.println(e1);
+        logger.info(e1);
 
         EventContextNetwork.ECNRef e2 = network.createEvent("photo-capture", 20, 30, "some place");
-        System.out.println(e2);
+        logger.info(e2);
 
         network.initializeSubeventTree(e1);
 
@@ -42,7 +45,7 @@ public class EventContextNetworkTester {
         }
 
         network.createSubeventEdge(e1, e2);
-        System.out.println("Subevent added");
+        logger.info("Subevent added");
 
         network.createPartiticipatingEdge(e1, p1);
         network.createPartiticipatingEdge(e1, p2);
@@ -57,9 +60,9 @@ public class EventContextNetworkTester {
 
         network.pruneUp();
 
-        System.out.println(".................");
+        System.out.println("-----------------");
         System.out.println(" After prune up ");
-        System.out.println(".................");
+        System.out.println("-----------------");
 
         network.printTree(true);
 
