25784974b5030223759984673f4cb6dc2cb3a0ce
wicknicks <arjun.satish@gmail.com>
2013-08-19 19:43:21 -0700

    edits

--- src/test/java/esl/cuenet/generative/PropagationTests.java
+++ src/test/java/esl/cuenet/generative/PropagationTests.java
@@ -11,6 +11,7 @@ import org.apache.log4j.Logger;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Set;
 
 public class PropagationTests {
@@ -35,14 +36,16 @@ public class PropagationTests {
         for (int i=1; i<=10; i++) entities[i-1] = "" + i;
         propagator.prepare(Sets.newHashSet(entities));
 
-        double l1delta = propagator.propagateOnce();
-        logger.info(l1delta);
-
+        double l1delta;
+        double[] deltas = new double[10];
 
         for (int i=0; i<10; i++) {
-            l1delta = propagator.propagateOnce(); logger.info(l1delta);
+            l1delta = propagator.propagateOnce();
+            logger.info(l1delta);
+            deltas[i] = l1delta;
         }
 
+        logger.info(Arrays.toString(deltas));
         //propagator.dispScores();
     }
 
